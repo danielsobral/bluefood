dfa26618a56149dd4b97d5ac1e506ceb948186dd
Daniel <danielsobralnascimento@hotmail.com>
2021-04-08 14:33:09 -0300

    Preparando upload de arquivos

--- src/main/java/com/daniel/bluefood/domain/application/RestauranteService.java
+++ src/main/java/com/daniel/bluefood/domain/application/RestauranteService.java
@@ -3,6 +3,7 @@ package com.daniel.bluefood.domain.application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.daniel.bluefood.domain.cliente.Cliente;
 import com.daniel.bluefood.domain.restaurante.Restaurante;
 import com.daniel.bluefood.domain.restaurante.RestauranteRepository;
 
@@ -11,31 +12,42 @@ public class RestauranteService {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
 	public void saveRestaurante(Restaurante restaurante) throws ValidationException {
-		
-		if(!validateEmail(restaurante.getEmail(), restaurante.getId())) {
+
+		if (!validateEmail(restaurante.getEmail(), restaurante.getId())) {
 			throw new ValidationException("E-mail já cadastrado");
 		}
-		
-		restauranteRepository.save(restaurante);
+
+		if (restaurante.getId() != null) {
+
+			Restaurante restauranteDB = restauranteRepository.findById(restaurante.getId()).orElseThrow();
+			restaurante.setSenha(restauranteDB.getSenha());
+
+		} else {
+			restaurante.ecncryptPassword();
+			restaurante = restauranteRepository.save(restaurante);
+			restaurante.setLogotipoFileName();
+			//TODO Upload!!
+		}
+
 	}
-	
+
 	public boolean validateEmail(String email, Integer id) {
-		
+
 		Restaurante restaurante = restauranteRepository.findByEmail(email);
-		
+
 		if (restaurante != null) {
 			if (id == null) {
 				return false;
 			}
-			
+
 			if (!restaurante.getId().equals(id)) {
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
+
 }
