2ddf9ece67c83142095d86c86459a3f57f1f6ad0
Daniel <danielsobralnascimento@hotmail.com>
2021-05-04 16:50:50 -0300

    Carrinho 1

---
+++ src/main/java/com/daniel/bluefood/domain/pedido/RestauranteDiferenteException.java
@@ -0,0 +1,16 @@
+package com.daniel.bluefood.domain.pedido;
+
+@SuppressWarnings("serial")
+public class RestauranteDiferenteException extends Exception {
+
+	public RestauranteDiferenteException() {
+		super();
+		
+	}
+
+	public RestauranteDiferenteException(String message) {
+		super(message);
+	
+	}
+
+}
